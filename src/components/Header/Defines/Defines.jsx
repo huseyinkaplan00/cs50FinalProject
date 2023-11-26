f94ade04342a188405b5e223afd7a36f5e9bc5c4
Huseyin Kaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-11-27 01:25:37 +0300

    Fix typo and remove unused code

--- src/components/Header/Defines/Defines.jsx
+++ src/components/Header/Defines/Defines.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 const Defines = ({ words, allCategories, meanings }) => {
   // getting link of the worlds to set src in a tags.
-
+  const [linkOfTheWords, setLinkOfTheWords] = React.useState("");
   return (
     <div className="defines">
       {words === "" ? (
         <span className="defines-title">Please Write Your Word to Search</span>
-      ) : (ca
+      ) : (
         meanings.map((means) => {
           // console.log(means.sourceUrls[0]);
           means.meanings.map((item) => {
-            item.definitions.map((def) => {
-              console.log(def.definition);
-            });
+            item.definitions.map((def) => console.log(def));
           });
         })
       )}
