babdd68eac80ed46dd89664b34375beb39aacff7
Huseyin Kaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-11-29 02:06:27 +0300

    displaying new datas

--- src/components/Header/Defines/Defines.jsx
+++ src/components/Header/Defines/Defines.jsx
@@ -4,30 +4,47 @@ import PropTypes from "prop-types";
 const Defines = ({ words, allCategories, meanings }) => {
   // getting link of the worlds to set src in a tags.
   const [linkOfTheWords, setLinkOfTheWords] = React.useState("");
+  const playAudio = (audio) => {
+    const audioLink = new Audio(audio);
+    audioLink.play();
+  };
   return (
     <div className="defines">
       {words === "" ? (
         <div>Search a word</div>
       ) : (
-        meanings.map((mean) =>
-          mean.meanings.map((item, index) => (
+        meanings.map((mean) => {
+          const wordsLink = mean.sourceUrls.map((item) => (item ? item : "#"));
+          const audioLink = mean.phonetics.map((item) => item.audio);
+
+          return mean.meanings.map((item, index) => (
             <div key={index}>
-              <h5 className="defines--part-of-spech">{item.partOfSpeech}</h5>
+              <div className="word__header">
+                <span className="defines--part-of-spech">
+                  <a href={wordsLink}> {mean.word} </a> (
+                  {item.partOfSpeech ? item.partOfSpeech : "🥲"})
+                </span>
+              </div>
               <div className="defines--definitions">
-                {item.definitions.map((def, index) =>
-                  console.log(index, def.definition)
-                )}
-                {/* {item.definitions.map((def, index) => (
-                  <div key={index} className="defines--definitions-words">
-                    {def.map((itemTwo, indexTwo) =>
-                      console.log(itemTwo, indexTwo)
+                {item.definitions.map((def, index) => (
+                  <div key={index} className="defines-definitions-single-word">
+                    <h4>Definition : </h4>
+                    {def.definition}
+                    <br />
+                    {def.example && (
+                      <div className="examples">
+                        <span className="examples">
+                          <b>Example:</b>
+                          <p>{def.example}</p>
+                        </span>
+                      </div>
                     )}
                   </div>
-                ))} */}
+                ))}
               </div>
             </div>
-          ))
-        )
+          ));
+        })
       )}
     </div>
   );
