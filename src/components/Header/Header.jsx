44df797eb01513d587c29befa29ca8b90d6cb47c
Huseyin Kaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-12-18 01:10:54 +0300

    Update styles in style.scss, _header.scss, _home.scss, and style.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,20 @@
-import React from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import TextField from "@mui/material/TextField";
-import languagesList from "../Data/languages";
+import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import languagesList from '../Data/languages';
 
-const Header = ({
-  allCategories,
-  setAllCategories,
-  words,
-  setWords,
-  darkMode,
-}) => {
+const Header = ({ allCategories, setAllCategories, words, setWords, darkMode }) => {
   const darkTheme = createTheme({
     palette: {
-      mode: darkMode ? "dark" : "light",
+      mode: darkMode ? 'dark' : 'light',
       primary: {
-        main: darkMode ? "#fff" : "#000",
-      },
-    },
+        main: darkMode ? '#fff' : '#000'
+      }
+    }
   });
 
   const [open, setOpen] = React.useState(false);
@@ -33,56 +27,56 @@ const Header = ({
     setOpen(true);
   };
 
-  const handleChangeCategory = (chosenLanguage) => {
+  const handleChangeCategory = chosenLanguage => {
     setAllCategories(chosenLanguage);
-    setWords("");
+    setWords('');
   };
   return (
-    <div className="header">
-      <a href="index.html">
-        {" "}
-        <h1 className="header__title"> {words ? words : "Your Dictionary"} </h1>
-      </a>
-      <div className="header__inputs">
-        <ThemeProvider theme={darkTheme}>
-          <FormControl>
-            <InputLabel id="demo-controlled-open-select-label">
-              Languages
-            </InputLabel>
-            <Select
-              labelId="demo-controlled-open-select-label"
-              id="demo-controlled-open-select"
-              open={open}
-              onClose={handleClose}
-              onOpen={handleOpen}
-              value={allCategories}
-              label="Languages"
-              onChange={(event) => handleChangeCategory(event.target.value)}
-            >
-              {languagesList.map((language) => (
-                <MenuItem value={language.label} key={language.label}>
-                  {language.value}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <TextField
-            sx={{
-              width: 300,
-              height: 50,
-            }}
-            fullWidth
-            id="filled-multiline-flexible"
-            label="Write Your Word"
-            multiline
-            maxRows={4}
-            value={words}
-            variant="filled"
-            onChange={(event) => setWords(event.target.value)}
-          />
-        </ThemeProvider>
+    <>
+      <div className="bg"></div>
+      <div className="header">
+        <a href="index.html">
+          {' '}
+          <h1 className="header__title"> {words ? words : 'Your Dictionary'} </h1>
+        </a>
+        <div className="header__inputs">
+          <ThemeProvider theme={darkTheme}>
+            <FormControl>
+              <InputLabel id="demo-controlled-open-select-label">Languages</InputLabel>
+              <Select
+                labelId="demo-controlled-open-select-label"
+                id="demo-controlled-open-select"
+                open={open}
+                onClose={handleClose}
+                onOpen={handleOpen}
+                value={allCategories}
+                label="Languages"
+                onChange={event => handleChangeCategory(event.target.value)}>
+                {languagesList.map(language => (
+                  <MenuItem value={language.label} key={language.label}>
+                    {language.value}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <TextField
+              sx={{
+                width: 300,
+                height: 50
+              }}
+              fullWidth
+              id="filled-multiline-flexible"
+              label="Write Your Word"
+              multiline
+              maxRows={4}
+              value={words}
+              variant="filled"
+              onChange={event => setWords(event.target.value)}
+            />
+          </ThemeProvider>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
