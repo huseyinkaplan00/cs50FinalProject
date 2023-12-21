732f77c27ebe05c197fa04e13b643a81e2508c39
Huseyin Kaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-12-21 13:04:44 +0300

    Remove unused code and optimize filtering of word meanings

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,137 +1,6 @@
-// import Marquee from 'react-marquee-slider';
-// import styled, { StyleSheetManager, createGlobalStyle } from 'styled-components';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import React from 'react';
-// import { MdArrowBack, MdArrowForward } from 'react-ionicons';
-
-//css
-
-//styled components: Global styles
-// const GlobalStyles = createGlobalStyle`
-// *
-// {
-//   margin: 0;
-// padding: 0;
-// box-sizing: border-box;
-// }
-
-// body{
-//   height: 100vh;
-//   display:grid;
-//   place-items: center;
-//   overflow:hidden;
-// }
-
-// `;
-
-// //styled components: Div styles
-// const Container = styled.main`
-//   position: relative;
-//   width: 100%;
-//   height: 100%;
-//   box-shadow: 0 3px 10px rgba(0, 0, 0, 0.3);
-// `;
-// const SliderContainer = styled.ul``;
-
-// const Item = styled.li`
-//   width: 200px;
-//   height: 300px;
-//   list-style-type: none;
-//   position: absolute;
-//   top: 50%;
-//   transform: translateY(-50%);
-//   z-index: 1;
-//   background-position: center;
-//   background-size: cover;
-//   border-radius: 20px;
-//   box-shadow: 0 20px 30px rgba(255, 255, 255, 0.3) inset;
-//   transition: transform 0.1s, left 0.75s, top 0.75s, width 0.75s, height 0.75s;
-
-//   &:nth-child(1),
-//   &:nth-child(2) {
-//     left: 0;
-//     top: 0;
-//     width: 100%;
-//     height: 100%;
-//     transform: none;
-//     border-radius: 0;
-//     box-shadow: none;
-//     opacity: 1;
-//   }
-
-//   &:nth-child(3) {
-//     left: 50%;
-//   }
-//   &:nth-child(4) {
-//     left: calc(50% + 220px);
-//   }
-//   &:nth-child(5) {
-//     left: calc(50% + 440px);
-//   }
-//   &:nth-child(6) {
-//     left: calc(50% + 660px);
-//     opacity: 0;
-//   }
-
-//   &:nth-of-type(2) {
-//     display: block;
-//     animation: show 0.75s ease-in-out 0.3s forwards;
-//   }
-// `;
-
-// const Content = styled.content`
-//   width: min(30vw, 400px);
-//   position: absolute;
-//   top: 50%;
-//   left: 3rem;
-//   transform: translateY(-50%);
-//   font: 400 0.85rem helvetica, sans-serif;
-//   color: white;
-//   text-shadow: 0 3px 8px rgba(0, 0, 0, 0.5);
-//   opacity: 0;
-//   display: none;
-
-//   & .title {
-//     font-family: 'arial-black';
-//     text-transform: uppercase;
-//   }
-
-//   & .description {
-//     line-height: 1.7;
-//     margin: 1rem 0 1.5rem;
-//     font-size: 0.8rem;
-//   }
-
-//   & button {
-//     width: fit-content;
-//     background-color: rgba(0, 0, 0, 0.1);
-//     color: white;
-//     border: 2px solid white;
-//     border-radius: 0.25rem;
-//     padding: 0.75rem;
-//     cursor: pointer;
-//   }
-// `;
-
-// const WordBox = styled.div`
-//   border-radius: 5px;
-//   display: grid;
-//   place-items: center;
-//   width: 330px;
-//   margin: 0 10px;
-//   height: 370px;
-//   background-color: #fff;
-//   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-//   background-image: url(${props => props.backgroundImage});
-//   color: black;
-//   background-size: cover;
-//   background-position: center;
-//   background-repeat: no-repeat;
-//   h3 {
-//     font-size: 2em;
-//   }
-// `;
 
 export default function Home({ wordMeanings, setWordMeanings, setWords }) {
   const activate = e => {
@@ -154,8 +23,12 @@ export default function Home({ wordMeanings, setWordMeanings, setWords }) {
     };
   }, []);
 
-  const filteredWordMeanings = wordMeanings.filter(
-    wordMeaning => wordMeaning.meaning !== 'Definition not available' && wordMeaning.image !== null
+  const filteredWordMeanings = React.useMemo(
+    () =>
+      wordMeanings.filter(
+        wordMeaning => wordMeaning.meaning !== 'Definition not available' && wordMeaning.image !== null
+      ),
+    [wordMeanings]
   );
 
   const getWordImage = async () => {
@@ -180,7 +53,7 @@ export default function Home({ wordMeanings, setWordMeanings, setWords }) {
   };
 
   React.useEffect(() => {
-    if (filteredWordMeanings && filteredWordMeanings.length > 0) {
+    if (filteredWordMeanings && filteredWordMeanings.length > 5) {
       getWordImage();
     }
   }, [filteredWordMeanings]);
@@ -211,8 +84,9 @@ export default function Home({ wordMeanings, setWordMeanings, setWords }) {
 }
 
 // // declaring prop types for type checking.
-Home.propTypes = {
-  wordMeanings: PropTypes.array.isRequired,
-  setWordMeanings: PropTypes.func.isRequired,
-  setWords: PropTypes.func.isRequired
-};
+// Home.propTypes = {
+//   wordMeanings: PropTypes.array.isRequired,
+//   setWordMeanings: PropTypes.func.isRequired,
+//   setWords: PropTypes.func.isRequired,
+//   filteredWordMeanings: PropTypes.array.isRequired
+// };
